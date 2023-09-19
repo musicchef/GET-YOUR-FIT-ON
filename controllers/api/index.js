fd6c83bb7c95f18e475e9a24f38bf0ca5eabdc12
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-09-19 14:56:06 -0700

    updates nutrition

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const exerciseRoutes = require('./exerciseRoutes');
-const foodRoutes = require('./foodRoutes');
+const nutritionRoutes = require('./nutritionRoutes');
 
 router.use('/users', userRoutes);
 router.use('/workouts', exerciseRoutes);
-router.use('/food', foodRoutes);
+router.use('/food', nutritionRoutes);
 
 module.exports = router;
