54b7e4dd760867f9a6e3971579a0ae17700fc96b
Hamza Lhamous <hamza.lhamous@gmail.com>
2023-09-19 10:49:58 -0700

    created User.js and Friend.js models, will add more to our Models index.js later to prevent any merge conflicts

---
+++ models/User.js
@@ -0,0 +1,76 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPassword) {
+        return bcrypt.compareSync(loginPassword, this.password);
+        // "this" is a user instance
+    }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: true, // We can change if we want names to be required
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: true, 
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // Ensure usernames are unique
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // Ensure emails are unique
+      unique: true,
+      // Validate email format
+      validate: {
+        isEmail: true, 
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8], 
+      },
+    },
+    profile_photo: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+   
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true, 
+    underscored: true,
+    modelName: 'User',
+    hooks: {
+        beforeCreate: async (newUser) => {
+            newUser.password = await bcrypt.hash(newUser.password, 10);
+            return newUser;
+        },
+        beforeUpdate: async (updatedUser) => {
+            updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
+            return updatedUser;
+        },
+    },
+  }
+);
+
+module.exports = User;
