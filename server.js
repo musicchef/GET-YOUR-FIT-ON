7a2566458185531974ae588e2ab1e341dfda3f20
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-09-20 18:34:40 -0700

    adds server

---
+++ server.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./')
