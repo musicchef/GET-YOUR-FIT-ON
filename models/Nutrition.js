b6f3234470c7913ae1f83be81def22360fbd9348
Nedda Elsayed <nedda_e@yahoo.com>
2023-09-20 20:40:18 -0700

    Nutrition updates

--- models/Nutrition.js
+++ models/Nutrition.js
@@ -4,28 +4,37 @@ const sequelize = require("../config/connection");
 class Nutrition extends Model {}
 
 Nutrition.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        food_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        calorie_count: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-    },
-    {
-        sequelize,
-        modelName: 'nutrition',
-        timestamps: true,
-        underscored: true,
-    }
+	{
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true,
+		},
+		food_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		calorie_count_per_serving: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		calorie_count_servings: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		meal_date: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: DataTypes.NOW,
+		},
+	},
+	{
+		sequelize,
+		modelName: "nutrition",
+		timestamps: true,
+		underscored: true,
+	}
 );
 
 module.exports = Nutrition;
