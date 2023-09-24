6db2dfa93b0a4ff1b7f1f7b8af2e4499d920a6f7
Hamza Lhamous <hamza.lhamous@gmail.com>
2023-09-23 17:07:17 -0700

    created script file to handle user.handlebars calculations, the index i added in the last pull requeset in user.handlebars is not necessary will fix after the pull requests have been merged

---
+++ public/js/userHandlebars.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Function to calculate calories burned
+    function calculateCaloriesBurned(exercise) {
+      const minutesElement = exercise.querySelector('.workoutMinutes');
+      const caloriesPerHourElement = exercise.querySelector('.caloriesPerHour');
+      const caloriesBurnedElement = exercise.querySelector('.caloriesBurned');
+      
+      const minutes = parseFloat(minutesElement.textContent);
+      const caloriesPerHour = parseFloat(caloriesPerHourElement.textContent);
+      
+      const caloriesBurned = (minutes / 60) * caloriesPerHour;
+      
+      caloriesBurnedElement.textContent = caloriesBurned.toFixed(2);
+    }
+    
+    // Function to calculate calories consumed
+    function calculateCaloriesConsumed(nutrition) {
+      const servingsElement = nutrition.querySelector('.servings');
+      const calsPerServingElement = nutrition.querySelector('.calsPerServing');
+      const caloriesConsumedElement = nutrition.querySelector('.caloriesConsumed');
+      
+      const servings = parseFloat(servingsElement.textContent);
+      const calsPerServing = parseFloat(calsPerServingElement.textContent);
+      
+      const caloriesConsumed = servings * calsPerServing;
+      
+      caloriesConsumedElement.textContent = caloriesConsumed.toFixed(2);
+    }
+    
+    // Get all exercise and nutrition list items
+    const exerciseItems = document.querySelectorAll('.user-activities li');
+    const nutritionItems = document.querySelectorAll('.user-nutrition li');
+    
+    // Calculate and update calories burned for each exercise
+    exerciseItems.forEach(function (exercise) {
+      calculateCaloriesBurned(exercise);
+    });
+    
+    // Calculate and update calories consumed for each nutrition item
+    nutritionItems.forEach(function (nutrition) {
+      calculateCaloriesConsumed(nutrition);
+    });
+  });
+  
