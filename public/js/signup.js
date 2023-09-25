d04008ed3ed2eb55063544ffeb057bc9390aa1f0
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-09-24 18:59:00 -0700

    updated user model to have alphanumeric for username, updated sign up handlebar and sign up js

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,25 +1,27 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
     console.log("Your button worked!")
-    // const username = document.querySelector('#name-signup').value.trim();
-    // const email = document.querySelector('#email-signup').value.trim();
-    // const password = document.querySelector('#password-signup').value.trim();
+    const firstname = document.querySelector('#firstname-input').value.trim();
+    const lastname = document.querySelector('#lastname-input').value.trim();
+    const username = document.querySelector('#username-input').value.trim();
+    const email = document.querySelector('#email-input').value.trim();
+    const password = document.querySelector('#password-input').value.trim();
   
-    // if (username && email && password) {
-    //   const response = await fetch('/api/user/signup', {
-    //     method: 'POST',
-    //     body: JSON.stringify({ username, email, password }),
-    //     headers: { 'Content-Type': 'application/json' },
-    //   });
+    if (username && email && password) {
+      const response = await fetch('/api/user/signup', {
+        method: 'POST',
+        body: JSON.stringify({ firstname, lastname, username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
   
-    //   if (response.ok) {
-    //     document.location.replace('/profile');
-    //   } else {
-    //     alert(response.statusText);
-    //   }
-    // }
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
   };
 
   document
-  .querySelector('#signup')
-  .addEventListener('click', signupFormHandler);
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
