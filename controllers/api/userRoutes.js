c57bfbdf9409aa004df61691c40c81e677c28fb5
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-09-24 20:05:15 -0700

    updated login and log out routes and handlebars

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -19,7 +19,7 @@ router.post('/signup', async (req, res) => {
 
   router.post('/login', async (req, res) => {
     try {
-      const userData = await User.findOne({ where: { username: req.body.username } });
+      const userData = await User.findOne({ where: { email: req.body.email } });
   
       if (!userData) {
         res
