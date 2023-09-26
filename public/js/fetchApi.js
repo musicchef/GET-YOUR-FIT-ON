576a29a511b6481346ffbe30a72e4a444376e123
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-09-25 20:24:17 -0700

    more work on api

--- public/js/fetchApi.js
+++ public/js/fetchApi.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const nutritionFormHandler = async (event) => {
     event.preventDefault();
     console.log("Your button worked!")
-    const userInput = document.getElementById('foodItem').value.trim();
+    const userInput = document.querySelector('#foodItem').value.trim();
 
     const fetchNutrition = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${process.env.NUTRITION_API_KEY}&query=${userInput}&pageSize=10`);
 
