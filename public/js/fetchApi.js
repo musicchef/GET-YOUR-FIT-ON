c3495776989f71caf4846d6ec724cc36970e2c2d
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-09-26 15:43:18 -0700

    adds nutrition lookup and shows api fetch in the dom.

--- public/js/fetchApi.js
+++ public/js/fetchApi.js
@@ -1,16 +1,60 @@
-const nutritionFormHandler = async (event) => {
+let root = document.getElementById('root')
+
+async function nutritionFormHandler(input) {
     event.preventDefault();
     console.log("Your button worked!")
+    const data = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=4xf4bbqjoGLfyf31OWdx7G3tGya1IgsrvJfScbhT&query=${input}`);
+    return data.json();
+};
+
+function clearDOM() {
+    root.textContent = ''
+}
+
+function getSearchResults () {
+    clearDOM()
+    let foodsArray = []
     const userInput = document.querySelector('#foodItem').value.trim();
+    nutritionFormHandler(userInput)
+    .then((result) => {
+        for(let i = 0; i < result.foods.length; i++) {
+            foodsArray.push(result.foods[i]);
+        }
+        console.log(foodsArray);
 
-    const fetchNutrition = await fetch(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=4xf4bbqjoGLfyf31OWdx7G3tGya1IgsrvJfScbhT&query=${userInput}&pageSize=10`);
+        let counter = 0;
+        foodsArray.forEach(element => {
+            if(element.foodNutrients[3].value == 0){
+                return;
+            }
+            let card = document.createElement('div');
+            let title = document.createElement('h3');
+            let calories = document.createElement('p');
+            let addFood = document.createElement('button');
 
-    let data = fetchNutrition.json()
-    console.log(data);
+            card.classList.add('w-25', 'mx-3', 'my-3', 'bg-light');
+            title.innerHTML = element.description;
+            card.append(title);
+            calories.innerHTML = `${element.foodNutrients[3].value} calories`;
+            card.append(calories)
+            addFood.innerHTML = 'Add to My Food List'
+            addFood.setAttribute('id',`uniqueID${counter}`)
+            card.append(addFood)
 
-    return data;
+            root.append(card)
+
+            counter++;
+        });
+    })
+    for(let i = 0; i < foodsArray.length; i++) {
+        document
+        .querySelector(`#uniqueID${i}`)
+        .addEventListener('click', (event) => {
+            console.log(this.calories)
+        })
+    }
 };
 
 document
 .querySelector('#search-nutrition')
-.addEventListener('submit', nutritionFormHandler);
+.addEventListener('submit', getSearchResults);
