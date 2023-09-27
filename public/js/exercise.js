8fe4d10fec4a82ce355585f21641d55625ce744a
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-09-27 15:42:38 -0700

    got exercise delete button working

--- public/js/exercise.js
+++ public/js/exercise.js
@@ -2,7 +2,7 @@ const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
         const id = event.target.getAttribute('data-id');
       
-        const response = await fetch(/api/workouts/id, {
+        const response = await fetch(`/api/workouts/${id}`, {
             method: 'DELETE',
         });
       
